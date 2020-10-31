05a9c9e00be32e934f4ffa980755c9262effb06e
Michael García <mgarcia@Michaels-MacBook-Pro-2.local>
2020-10-30 22:39:04 -0500

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,89 @@
+const keys = require('./keys');
+
+// =======================================================================================================
+// Express App Setup =====================================================================================
+// =======================================================================================================
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// app: an object that will receive and respond to any HTTP request
+const app = express();
+
+// cors: cross-origin resource sharing.
+// Allow us to make request from one domain (React) to a different domain (Express API)
+app.use(cors());
+
+// bodyParser: parse incoming request from React and turn the body of post into a JSON value
+app.use(bodyParser.json());
+
+// =======================================================================================================
+// Postgres Client Setup =================================================================================
+// =======================================================================================================
+
+const { Pool } = require('pg');
+const pgClient = new Pool({
+	user: keys.pgUser,
+	host: keys.pgHost,
+	database: keys.pgDatabase,
+	password: keys.pgPassword,
+	port: keys.pgPort,
+});
+
+pgClient.on('connect', () => {
+	pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)').catch(err => console.log(err));
+});
+
+// =======================================================================================================
+// Redis Client Setup ====================================================================================
+// =======================================================================================================
+
+const redis = require('redis');
+const redisClient = redis.createClient({
+	host: keys.redisHost,
+	port: keys.redisPort,
+	retry_strategy: () => 1000,
+});
+
+// why duplicate()? If we have a client listening/publishing we have to make a duplicate connection because
+// it cannot be used for both purposes.
+const redisPublisher = redisClient.duplicate();
+
+// =======================================================================================================
+// Express Route Handlers ================================================================================
+// =======================================================================================================
+
+app.get('/', (req, res) => {
+	res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+	const values = await pgClient.query('SELECT * from values');
+
+	res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+	redisClient.hgetall('values', (err, values) => {
+		res.send(values);
+	});
+});
+
+app.post('/values', async (req, res) => {
+	const index = req.body.index;
+
+	if (parseInt(index) > 40) {
+		return res.status(422).send('Index too high');
+	}
+
+	redisClient.hset('values', index, 'Nothing yet!');
+	redisPublisher.publish('insert', index);
+	pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+	res.send({ working: true });
+});
+
+app.listen(5000, err => {
+	console.log('Listening');
+});
